c8dfc21046079128496ca5893bfad3f3fd44e2b5
Edoardo Vacchi <uncommonnonsense@gmail.com>
2014-11-14 17:24:58 +0100

    updated files with licensing info

--- src/main/java/org/tylproject/vaadin/addon/package-info.java
+++ src/main/java/org/tylproject/vaadin/addon/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 - Marco Pancotti, Edoardo Vacchi, Daniele Zonca
+ * Copyright (c) 2014 - Marco Pancotti, Edoardo Vacchi and Daniele Zonca
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
